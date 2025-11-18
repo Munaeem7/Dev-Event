499409ab4849acd0dd5ec524168daf82648c2323
Munaeem7 <munaeemm2@gmail.com>
2025-11-19 02:40:25 +0500

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,61 @@
+export type EventItem = {
+    image : string,
+    title : string,
+    slug: string,
+    location : string,
+    date : string,
+    time : string
+}
+
+
+export  const  events: EventItem[] = [
+    {
+        image: "/images/event1.png",
+        title: "React Summit US 2025",
+        slug: "react-summit-us-2025",
+        location: "San Francisco, CA, USA",
+        date: "2025-11-07",
+        time: "09:00 AM"
+    },
+    {
+        image: "/images/event2.png",
+        title: "AWS re:Invent 2025",
+        slug: "aws-reinvent-2025",
+        location: "Las Vegas, NV, USA",
+        date: "2025-12-01",
+        time: "08:30 AM"
+    },
+    {
+        image: "/images/event3.png",
+        title: "Google I/O 2025",
+        slug: "google-io-2025",
+        location: "Mountain View, CA, USA",
+        date: "2025-05-14",
+        time: "10:00 AM"
+    },
+    {
+        image: "/images/event4.png",
+        title: "Microsoft Build 2025",
+        slug: "microsoft-build-2025",
+        location: "Seattle, WA, USA",
+        date: "2025-05-21",
+        time: "09:00 AM"
+    },
+    {
+        image: "/images/event5.png",
+        title: "Apple WWDC 2025",
+        slug: "apple-wwdc-2025",
+        location: "Cupertino, CA, USA",
+        date: "2025-06-09",
+        time: "10:00 AM"
+    },
+    {
+        image: "/images/event6.png",
+        title: "Vue.js Nation 2025",
+        slug: "vuejs-nation-2025",
+        location: "Paris, France",
+        date: "2025-01-24",
+        time: "09:30 AM"
+    },
+
+]
